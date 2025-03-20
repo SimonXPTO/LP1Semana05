984f558af71cac9cdb3317fa585ff6ea26ba3d33
SimonXPTO <simaodurao@gmail.com>
2025-03-20 21:03:19 +0000

    Added Images and LevelTwo

---
+++ LevelTwo/Program.cs
@@ -0,0 +1,18 @@
+﻿using Spectre.Console;
+
+public static class Program
+{
+    private static void Main(string[] args)
+    {
+        CanvasImage image =new CanvasImage("tux.jpg");
+        int maxWidth = 20;
+
+
+        if (args.Length >= 2)
+        {
+            image = new CanvasImage(args[0]); 
+            maxWidth = int.Parse(args[1]);
+        }
+        AnsiConsole.Write(image);
+    }
+}
