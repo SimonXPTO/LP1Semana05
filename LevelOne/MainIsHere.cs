a459168cfd341620807208185ccbafaf092375fa
SimonXPTO <simaodurao@gmail.com>
2025-03-20 20:36:50 +0000

    Add color print

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -16,7 +16,7 @@ namespace LevelOne
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
-            Console.WriteLine("Bad day?") ;
+            Console.WriteLine("Nice day!") ;
             Console.WriteLine("Bye!");
         }
     }
