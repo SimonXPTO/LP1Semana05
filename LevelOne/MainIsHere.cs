10625842a3bde6f319019d9ebf31cba2b07837a3
SimonXPTO <simaodurao@gmail.com>
2025-03-20 20:17:12 +0000

    New method

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -4,6 +4,13 @@ namespace LevelOne
 {
     public class MainIsHere
     {
+        /// <summary>
+        /// Does a cool thing.
+        /// </summary>
+        /// <param name="args"></param> <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
