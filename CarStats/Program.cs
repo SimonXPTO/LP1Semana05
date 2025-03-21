3201ea0bc1e36210292fee6b38bc6f15863ada08
SimonXPTO <simaodurao@gmail.com>
2025-03-21 17:41:58 +0000

    Added Faker.Vehiclue and Faker.Random to bc

--- CarStats/Program.cs
+++ CarStats/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Spectre.Console;
+using Bogus;
+using System.ComponentModel;
 
 namespace CarStats
 {
@@ -6,7 +9,23 @@ namespace CarStats
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Randomizer.Seed = new Random(int.Parse(args[0]));
+            Faker faker = new Faker();
+
+            BarChart bc = new BarChart();
+
+            string brand = new Faker().Vehicle.Manufacturer();
+            int count = new Faker().Random.Number(1,20);
+
+            AnsiConsole.Write(bc
+                .Label("Cars sales")
+                .CenterLabel()
+                .AddItem(brand, count)
+                .AddItem(faker.Vehicle.Manufacturer(), faker.Random.Number(1,20))
+                .AddItem(faker.Vehicle.Manufacturer(), faker.Random.Number(1,20))
+            );
+
+            
         }
     }
 }
